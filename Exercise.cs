72ae6c984bc11b5af6de1b2b19ad4bc0eb59e2ce
Volter <gusha987@gmail.com>
2024-04-17 13:48:07 +0300

    added

---
+++ Exercise.cs
@@ -0,0 +1,22 @@
+﻿namespace bstproject
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime LastEdited { get; set; }
+        public DateTime LastUsed { get; set; }
+        public Boolean IsFavorite { get; set; }
+
+        public Exercise(int id, string name, string url)
+        {
+            Id = id;
+            Name = name;
+            Url = url;
+            LastEdited = DateTime.Now;
+            LastUsed = DateTime.Now;
+            IsFavorite = false;
+        }
+    }
+}
