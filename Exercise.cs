0684e1429bc74129249a1302b1e9a9fe5e58acef
Volter <gusha987@gmail.com>
2024-05-08 02:47:56 +0300

    added user switch and user functionality

--- Exercise.cs
+++ Exercise.cs
@@ -5,18 +5,16 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
-        public DateTime LastEdited { get; set; }
-        public DateTime LastUsed { get; set; }
-        public Boolean IsFavorite { get; set; }
+        public int UserId { get; set; } // Owner user ID
+        public bool IsPublic { get; set; } // Visibility flag
 
-        public Exercise(int id, string name, string url)
+        public Exercise(int id, string name, string url, int userId, bool isPublic)
         {
             Id = id;
             Name = name;
             Url = url;
-            LastEdited = DateTime.Now;
-            LastUsed = DateTime.Now;
-            IsFavorite = false;
+            UserId = userId;
+            IsPublic = isPublic;
         }
     }
 }
